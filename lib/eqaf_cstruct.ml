c6c2029235ade78c212b68abd017912da85de772
Pierre Alain <pierre.alain@tuta.io>
2024-01-25 10:55:19 +0100

    cstruct is baked by bytes and not anymore bigarrays

--- lib/eqaf_cstruct.ml
+++ lib/eqaf_cstruct.ml
@@ -1,26 +1,26 @@
 let equal a b =
-  Eqaf_bigstring.equal (Cstruct.to_bigarray a) (Cstruct.to_bigarray b)
+  Eqaf.equal (Cstruct.to_string a) (Cstruct.to_string b)
 
 let compare_be_with_len ~len a b =
-  Eqaf_bigstring.compare_be_with_len ~len
-    (Cstruct.to_bigarray a) (Cstruct.to_bigarray b)
+  Eqaf.compare_be_with_len ~len
+    (Cstruct.to_string a) (Cstruct.to_string b)
 
 let compare_le_with_len ~len a b =
-  Eqaf_bigstring.compare_le_with_len ~len
-    (Cstruct.to_bigarray a) (Cstruct.to_bigarray b)
+  Eqaf.compare_le_with_len ~len
+    (Cstruct.to_string a) (Cstruct.to_string b)
 
 let compare_le a b =
-  Eqaf_bigstring.compare_le
-    (Cstruct.to_bigarray a) (Cstruct.to_bigarray b)
+  Eqaf.compare_le
+    (Cstruct.to_string a) (Cstruct.to_string b)
 
 let compare_be a b =
-  Eqaf_bigstring.compare_be
-    (Cstruct.to_bigarray a) (Cstruct.to_bigarray b)
+  Eqaf.compare_be
+    (Cstruct.to_string a) (Cstruct.to_string b)
 
 let find_uint8 ?off ~f v =
-  Eqaf_bigstring.find_uint8
-    ?off ~f (Cstruct.to_bigarray v)
+  Eqaf.find_uint8
+    ?off ~f (Cstruct.to_string v)
 
 let exists_uint8 ?off ~f v =
-  Eqaf_bigstring.exists_uint8
-    ?off ~f (Cstruct.to_bigarray v)
+  Eqaf.exists_uint8
+    ?off ~f (Cstruct.to_string v)
